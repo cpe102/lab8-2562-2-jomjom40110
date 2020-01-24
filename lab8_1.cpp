b0863f74c1f9c6ebe63fa00799a87fce02c5bacd
jomjom40110 <jomjom40110@gmail.com>
2020-01-24 16:19:09 +0700

    lab8

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){	
+	float loan,rpy,Payment,Total,NewBalance,Interest,PrevBalance;
+	cout << "Enter initial loan: ";
+	cin >> PrevBalance;
+	cout << "Enter interset rate per year (%): ";
+	cin >> Interest;
+	Interest=Interest/100;
+	cout << "Enter amount you can pay per year: ";
+	cin >> Payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -14,17 +22,24 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	NewBalance=PrevBalance;
+	for(int year=1;NewBalance>0;year++){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left <<	PrevBalance;
+	cout << setw(13) << left << Interest*PrevBalance;
+	Total=(Interest*PrevBalance)+PrevBalance;
+	cout << setw(13) << left << Total;
+	if(Total<Payment) Payment=Total;
+	cout << setw(13) << left << Payment;
+	NewBalance=Total-Payment;
+	cout << setw(13) << left << NewBalance;
+	cout << "\n";	
+	PrevBalance=NewBalance;
+	}
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int a,int b){//a = line , b =O
 
+if(a<=0||b<=0)
+{
+	cout<<"Invalid input";
+}else{	
+	for(int i=0;i<a;i++)
+	{
+		for(int j=0;j<b;j++)
+		{
+			cout << 'O';
+		}
+		cout << '\n';
+	}
+	}
+}
 int main(){
 	
 	printO(2,2);
